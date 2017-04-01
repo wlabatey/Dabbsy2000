cf46b393d8f8cd5f595eb1eedd74c20393ee6dee
Will Batey <will@willbatey.co.uk>
2017-04-02 00:43:57 +0100

    Added nuke command, technical term detection & randomly timed messages.

--- bot/bot.js
+++ bot/bot.js
@@ -8,17 +8,17 @@ Dabbsy2000 is a Chris Dabbs simulator based on a fork of the node-irc library by
 TO DO:
 
     Basic:
-        - Greeting on join
-        - Detect technical terms and respond
-        - Nuke command
-        - Randomly timed messages
-        - Op command
-        - Check auth (based on whois data and/or nickname)
 
-    Advanced:
-        - Add / remove users from auth'd users list
-        - More complete response chains / conversations
-        - Meeting mode 
+        - Op command / auto Op (Think about best way to implement. Either use promises or refactor..)
+
+        - Add / list / remove quotes
+        - Check auth & auto op authorised users (based on whois data and nickname)
+        - Detect changes to github repositories and announce to channel
+
+        - Greeting on join (done)
+        - Detect technical terms and respond (done)
+        - Nuke command (done)
+        - Randomly timed messages (done)
 
 */
 
@@ -43,6 +43,9 @@ const BOT = new IRC.Client('chat.freenode.net', 'Dabbsy2000',
 // Categorise people by nickname
 
 const PEOPLE = {
+    admins: [
+        'BillTheBloke'
+    ],
     devs: [
         'BillTheBloke',
         'SimoneLivngstone',
@@ -62,12 +65,30 @@ const DABBSY = {
     methods: {
         // Kick everybody else from channel
         nuke(from) {
+            let channel_users = BOT.chans['#wangerz'].users;
+            let nicks_to_kick = [];
 
+            console.log(nicks_to_kick);
+            console.log(BOT.nick);
+
+            for (nick in channel_users) {
+                if (nick !== BOT.nick) {
+                    nicks_to_kick.push(nick);
+                }
+            }
+
+            nicks_to_kick.forEach((nick) => {
+                BOT.send('KICK', DABBSY.channel, nick, `NUKED BY ${from}`);
+            });
         },
 
         // Give operator status
-        op(from) {
-
+        op(nick) {
+                console.log(isAnAdmin(nick));
+                if (isAnAdmin(nick)) {
+                    console.log(isAnAdmin(nick));
+                    BOT.send('MODE', DABBSY.channel, '+o', nick);
+                } 
         },
         // Dance!!!
         dance(channel) {
@@ -78,10 +99,11 @@ const DABBSY = {
         }
     },
     responses: {
-        greetings: [
-            'Hellooooo!',
-            'Helloo Helloo Helloo!',
-            'Hi how\'s it going?',
+        excuses: [
+            'I had to walk the cat!',
+            'I literally just got back from America!',
+            'I just got off the phone with the guys at Google!',
+            'I\'ve literally just gotten out of a meeting!',
         ],
         quotes: [
             'What\'s the ping rate onto that?',
@@ -106,10 +128,10 @@ const DABBSY = {
             dance: ['!dance'],
         },
         greetings: [
-            'Hello',
-            'Hi',
-            'Hola',
-            'Alright',
+            'hello',
+            'hi',
+            'hola',
+            'alright',
         ],
         technicalTerms: [
             'database',
@@ -121,7 +143,7 @@ const DABBSY = {
             'CDN',
             'wordpress',
             'server',
-            'C#',
+            'c#',
             'c++',
             'php',
             'html',
@@ -140,13 +162,20 @@ const DABBSY = {
     }
 }
 
+// --------------------------------------------------------------------------------------------------------------
+
+// --- Functions ---
+
 // Check user's whois data and/or nickname against admin list and return true or false
 
-// function isAdmin(from) {
-//     BOT.whois(from, function() {
-//         console.log(this)
-//     })
-// }
+
+// -----> This needs to be called using a promise 
+
+// https://developers.google.com/web/fundamentals/getting-started/primers/promises
+
+function isAnAdmin(nick) {
+    let info = BOT.whois(nick, (info) => PEOPLE.admins.includes(info.account));
+}
 
 function randomItemFromArray(responseArray) {
     return responseArray[Math.floor(Math.random() * responseArray.length)];
@@ -158,6 +187,15 @@ function randomTimeBetween(min, max) {
     console.log(time);
     return time;
 }
+
+// Splits a string into an array containing each word and then checks 
+// if any element of that array matches any element of another array
+function searchArrayForString(string, array) {
+    let stringArr = string.split(' ');
+    return stringArr.some((item) => array.includes(item.toLowerCase())); 
+}
+
+// --------------------------------------------------------------------------------------------------------------
 
 BOT.addListener('error', function(message) {
     console.error(`ERROR: ${message.command}: ${message.args.join(' ')}`);
@@ -173,70 +211,51 @@ BOT.addListener('message', function(from, to, message) {
     // Message to channel that includes the command prefix '!'
     if (to.match(/^[#&]/) && message.match(/^[!&]/)) {
 
-
         if (DABBSY.triggers.commands.nuke.includes(message)) {
-            let channel_users = BOT.chans['#wangerz'].users;
-            let nicks_to_kick = [];
-
-            console.log(nicks_to_kick);
-            console.log(BOT.nick);
-
-            for (nick in channel_users) {
-                if (nick !== BOT.nick) {
-                    nicks_to_kick.push(nick);
-                }
-            }
-
-            nicks_to_kick.forEach((nick) => {
-                BOT.send('KICK', DABBSY.channel, nick, `NUKED BY ${from}`);
-            });
+            DABBSY.methods.nuke(to);
+            return;
         }
 
         if (DABBSY.triggers.commands.dance.includes(message)) {
             DABBSY.methods.dance(to);
+            return;
         }
-
-        // BOT.say(to, 'Sorry I don\'t recognise that command!');
     }
 
     // Message to channel that includes 'Dabbsy2000'
     if (to.match(/^[#&]/) && message.includes(DABBSY.name)) {
-        console.log("Message to Dabbsy2000!");
-        // channel message to bot
-        // if (message.includes(DABBSY.triggers.technicalTerms.forEach((el) => el))) {
-        // // })       DABBSY.triggers.technicalTerms.includes(message)) {
-        //     BOT.say(to, DABBSY.responses.quotes[0]);
-        //     return;
-        // }
-        // if (DABBSY.triggers.greetings.includes(message)) {
-        //     console.log('greeting detected!');
-        //     BOT.say(to, greeting(from, DABBSY.responses.greetings));
-        //     return;
-        // }
 
-        if (message.includes('hi') || message.includes('hello')) {
+        if (searchArrayForString(message, DABBSY.triggers.greetings)) { 
             BOT.say(to, `Hellooo ${from}!`);
             return;
         }
-        if (message.includes('dance')) {
-            Dabbsy.methods.dance(to);
-        }
     }
-    else {
-        // private message
-        console.log('private message');
+
+    // Message to channel
+    if (to.match(/^[#&]/)) {
+
+        // Message to channel contains item from technical terms array, randomised to occur 2/5 of the time
+        if (searchArrayForString(message, DABBSY.triggers.technicalTerms) && Math.random() > 0.6) {
+            BOT.say(to, randomItemFromArray(DABBSY.responses.error));
+            return;
+        }
     }
 });
 
 BOT.addListener('pm', function(nick, message) {
     console.log(`Got private message from ${nick}: ${message}`);
+
+    if (DABBSY.triggers.commands.op.includes(message)) {
+        DABBSY.methods.op(nick);
+    }
 });
 
 BOT.addListener('join', function(channel, who) {
     if (who === DABBSY.name) {
-        BOT.say(channel, 'Hellooo Hellooo Helloooo! Sorry I\'m late, I had to walk the cat!');
+        BOT.say(channel, `Helloooo! Sorry I'm late ${randomItemFromArray(DABBSY.responses.excuses)}`);
     } else {
         BOT.say(channel, `Helloooooo ${who}!`);
+        DABBSY.methods.op(who);
     }
 });
 
@@ -248,11 +267,12 @@ BOT.addListener('kick', function(channel, who, by, reason) {
     console.log(`${who} was kicked from ${channel} by ${by}: ${reason}`);
 });
 
-// Repeatedly say quotes at random intervals
-// For now, just says something every 15 minutes...
-
-global.setInterval(() => {
-
-    BOT.say(DABBSY.channel, randomItemFromArray(DABBSY.responses.quotes));
-
-}, 600000);
+// Say random quote at a random interval between 15 and 60 minutes
+(function loop() {
+    let rand = randomTimeBetween(15, 60);
+    console.log(rand);
+    setTimeout(function() {
+        BOT.say(DABBSY.channel, randomItemFromArray(DABBSY.responses.quotes));
+        loop();      
+    }, rand);
+})();
